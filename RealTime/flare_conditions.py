448c824ce9226a3fac9f0f597c5e896ef3d768d6
pet00184 <90334148+pet00184@users.noreply.github.com>
2023-07-10 11:33:37 -0500

    Update flare_conditions.py
    Trying a C2.3 threshold for flare end.

--- RealTime/flare_conditions.py
+++ RealTime/flare_conditions.py
@@ -6,7 +6,7 @@ def flare_trigger_condition(xrsa_data, xrsb_data):
     such changes. '''
     a = xrsa_data['flux']
     b = xrsb_data['flux']
-    flux_val = 2.5e-6
+    flux_val = 2.3e-6
     flux_deriv_val = 1e-9
     return b.iloc[-1] > flux_val and (b.iloc[-1] - b.iloc[-2]) > 0 #a.iloc[-1] - a.iloc[-1] > flux_deriv_val
     
@@ -14,6 +14,6 @@ def flare_end_condition(xrsa_data, xrsb_data):
     ''' Condition that signifies a flare has ended.'''
     a = xrsa_data['flux']
     b = xrsb_data['flux']
-    flux_val = 2.5e-6
+    flux_val = 2.3e-6
     flux_deriv_val = 1e-9
     return b.iloc[-1] < flux_val #(b.iloc[-1] < flux_val) and (a.iloc[-1] - a.iloc[-2] < flux_deriv_val)
