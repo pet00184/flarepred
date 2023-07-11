Y-lims were unintentionaly fixed at the start of plotting, scaling now happens if it needs to.

RealTime/realtime_flare_trigger.py
@@ -116,11 +116,8 @@ class RealTimeTrigger(QtWidgets.QWidget):
         log_value = [-10, -9, -8, -7, -6, -5, -4, -3, -2, -1]
         goes_labels = ["A0.01", "A0.1", "A1", "B1", "C1", "M1", "X1", "X10", "X100", "X1000"]
 
-        # depend plotting on lowest ~A1 (slightly less to make sure tick plots)
-        self.lower = np.max([log_value[2]*1.02, np.log10(np.min(self.xrsa['flux']))-1])
-        # on 200x largest xsrb value to look sensible and scale with new data
-        self.upper = np.min([np.log10(np.max(self.xrsb['flux']))+2, -3])
-        self.graphWidget.plotItem.vb.setLimits(yMin=self.lower, yMax=self.upper)
+        # set the y-limits for the plot
+        self.ylims()
 
         # do axis stuff, show top line and annotate right axis
         self.graphWidget.showAxis('top')
@@ -131,6 +128,13 @@ class RealTimeTrigger(QtWidgets.QWidget):
         self.graphWidget.getAxis('right').setTicks([[(v, str(s)) for v,s in zip(log_value,goes_labels)]])
         self.graphWidget.getAxis('right').setGrid(False)
         self.graphWidget.getAxis('left').setTicks([[(v, str(s)) for v,s in zip(log_value,value)]])
+
+    def ylims(self):
+        # depend plotting on lowest ~A1 (slightly less to make sure tick plots)
+        self.lower = np.max([-8*1.02, np.log10(np.min(self.xrsa['flux']))-0.5]) # *1.02 to make sure lower tick for -8 actually appears if needed
+        # on 200x largest xsrb value to look sensible and scale with new data
+        self.upper = np.min([np.log10(np.max(self.xrsb['flux']))+0.5, -3*0.96]) # *0.96 to make sure upper tick for -3 actually appears if needed
+        self.graphWidget.plotItem.vb.setLimits(yMin=self.lower, yMax=self.upper)
 
     def flare_prediction_state(self, state):
         self._flare_prediction_state = state
@@ -179,6 +183,9 @@ class RealTimeTrigger(QtWidgets.QWidget):
             self.new_data = True
             if self.print_updates: print(f'{new_minutes} new minute(s) of data: most recent data from {self.current_time}')
             if self.print_updates and new_minutes > 1: print('More than one minute added! Check internet connection.')
+
+            # make sure the y-limits change with the plot if needed and alert that new data is added
+            self.ylims()
             self.value_changed_new_xrsb.emit()
             
     def check_for_trigger(self):