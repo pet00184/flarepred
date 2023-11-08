1d9b24545aaa60de130b9adb84a87ea863e8de54
Kristopher Cooper <kris@infra04-wg012.eduroam.wireless.umn.edu>
2023-11-08 16:41:32 -0600

    can now easily add new conditions to the gui just by editing the flare conditions module

--- RealTime/realtime_flare_trigger.py
+++ RealTime/realtime_flare_trigger.py
@@ -110,9 +110,8 @@ class RealTimeTrigger(QtWidgets.QWidget):
 
         # alerts *** DO NOT forget to end both tuples with `,`
         # add new alerts to `update_flare_alerts()` as well
-        self.flare_alert_names = ('XRSB>C2.3',) #
-        self.flare_alerts =  np.zeros(1, np.dtype({'names':self.flare_alert_names, 
-                                                   'formats':('bool',)*len(self.flare_alert_names)}))
+        self.flare_alert_names = tuple(fc.FLARE_ALERT_MAP.keys())
+        self.flare_alerts = pd.DataFrame(data={n:[False] for n in self.flare_alert_names}, index=["states"])
         
         #updating data
         self.timer = QtCore.QTimer()
@@ -277,13 +276,15 @@ class RealTimeTrigger(QtWidgets.QWidget):
             self.display_goes()
             self.value_changed_new_xrsb.emit()
 
-    def update_flare_alerts(self):     
-        self.flare_alerts['XRSB>C2.3'] = fc.flare_trigger_condition(xrsa_data=self.xrsa, xrsb_data=self.xrsb)   
+    def update_flare_alerts(self):  
+        """ Function to update the alerts and emit a signal. """
+        for a in self.flare_alert_names:
+            self.flare_alerts.at['states', a] = fc.FLARE_ALERT_MAP[a](xrsa_data=self.xrsa, xrsb_data=self.xrsb)  
         self.value_changed_alerts.emit()
     
     def check_for_trigger(self):
         self.update_flare_alerts()
-        if np.all(self.flare_alerts):
+        if np.all(self.flare_alerts.loc['states']):
             self.change_to_triggered_state()
             self.flare_summary.loc[self.flare_summary_index, 'Trigger'] = self.current_time
             self.flare_summary.loc[self.flare_summary_index, 'Realtime Trigger'] = self.current_realtime
